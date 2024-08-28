4b1f3b5c68f08872fb5ce4f5a437292937c04bdd
Ovesh Mulla <oveshmulla1124@gmail.com>
2024-08-28 15:28:39 +0530

    mobile responsive_4

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -158,9 +158,7 @@ const SearchBar = ({ type }: { type?: string }) => {
             alt="mic icon"
             width={18}
             height={26}
-            className={`absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 cursor-pointer mr-3 ${
-              type ? "hidden" : "block"
-            }`}
+            className={`absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 cursor-pointer mr-3`}
           />
         </div>
       </form>
