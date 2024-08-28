e6f5494fb3873b781e77ccc05ab1aa9c1954bb73
Ovesh Mulla <oveshmulla1124@gmail.com>
2024-08-28 12:46:02 +0530

    loading skeleton added

--- app/GetWeatherData/[localityId]/loading.tsx
+++ app/GetWeatherData/[localityId]/loading.tsx
@@ -3,15 +3,26 @@ import Script from 'next/script';
 
 export default function Loader() {
   return (
-    <div className="flex justify-center items-center min-h-screen">
-      {/* Load the ldrs library */}
-      <Script 
-        type="module" 
-        src="https://cdn.jsdelivr.net/npm/ldrs/dist/auto/waveform.js" 
-        strategy="afterInteractive"
-      />
-      {/* Loader Element */}
-      <l-waveform size="35" stroke="3.5" speed="1" color="black"></l-waveform>
+    <div className="p-4 space-y-4">
+    {/* Search Bar Skeleton */}
+    <div className="flex justify-center">
+      <div className="w-full max-w-xl">
+        <div className="w-full h-12 bg-gray-300 rounded-full animate-pulse"></div>
+      </div>
     </div>
+
+    {/* Cards Skeleton */}
+    <div className="space-y-4">
+      {Array.from({ length: 5 }).map((_, index) => (
+        <div
+          key={index}
+          className="w-full max-w-xl mx-auto p-4 bg-gray-300 rounded-lg animate-pulse"
+        >
+          <div className="h-4 bg-gray-400 rounded w-3/4 mb-2"></div>
+          <div className="h-4 bg-gray-400 rounded w-1/2"></div>
+        </div>
+      ))}
+    </div>
+  </div>
   );
 }
