06dd81e8b54d901c33651f6b6f57f9ddedb100e5
Ovesh Mulla <oveshmulla1124@gmail.com>
2024-08-29 12:18:27 +0530

    bug fixes in env variable

--- app/GetWeatherData/[localityId]/page.tsx
+++ app/GetWeatherData/[localityId]/page.tsx
@@ -7,7 +7,7 @@ const URL = process.env.NEXT_PUBLIC_WEATHER_URL;
 
 async function getWeatherData(localityId: string) {
   const data = await fetch(
-    `https://www.weatherunion.com/gw/weather/external/v0/get_locality_weather_data?locality_id=${localityId}`,
+    `${URL}${localityId}`,
     {
       headers: {
         "X-Zomato-Api-Key": API_KEY || "",
